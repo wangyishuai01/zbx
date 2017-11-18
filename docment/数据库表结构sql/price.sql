eece493e695c2e57ff42e6c532b1acebaee64be8
zhangdongliang <1061650793@qq.com>
2017-11-18 18:59:10 +0800

    zdl 修改价格金额数据类型

--- docment/数据库表结构sql/price.sql
+++ docment/数据库表结构sql/price.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50139
 File Encoding         : 65001
 
-Date: 2017-10-28 22:36:27
+Date: 2017-11-18 03:13:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,9 +23,9 @@ CREATE TABLE `price` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` int(1) DEFAULT NULL COMMENT '价格类型：1：文章 2：视屏',
   `product_id` int(11) DEFAULT NULL COMMENT '文章id或视屏id',
-  `price` int(11) DEFAULT '0' COMMENT '单位：元',
+  `price` double(11,2) DEFAULT '0.00' COMMENT '单位：元',
   `play_number` int(11) DEFAULT '0',
-  `sum_price` int(11) DEFAULT '0' COMMENT '总钱数：单位：元',
+  `sum_price` double(11,2) DEFAULT '0.00' COMMENT '总钱数：单位：元',
   `again_pay_days` int(11) DEFAULT '0' COMMENT '单位：天',
   `makedate` datetime NOT NULL,
   `modifydate` datetime NOT NULL,
@@ -35,3 +35,4 @@ CREATE TABLE `price` (
 -- ----------------------------
 -- Records of price
 -- ----------------------------
+INSERT INTO `price` VALUES ('1', '2', '5', '1.00', '66', '66.00', '10', '2017-11-18 03:12:44', '2017-11-18 03:12:48');
