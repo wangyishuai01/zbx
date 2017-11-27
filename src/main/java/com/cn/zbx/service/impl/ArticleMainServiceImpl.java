ed7c934ee620c734b28dc15af47ed2abd7099643
zhangdongliang <1061650793@qq.com>
2017-11-27 16:23:29 +0800

    解决冲突 zdl

--- src/main/java/com/cn/zbx/service/impl/ArticleMainServiceImpl.java
+++ src/main/java/com/cn/zbx/service/impl/ArticleMainServiceImpl.java
@@ -12,14 +12,19 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cn.zbx.dao.ArticleMainMapper;
+import com.cn.zbx.dao.KeyRelationMapper;
+import com.cn.zbx.dao.KeyWordsMapper;
 import com.cn.zbx.dao.PriceMapper;
 import com.cn.zbx.pojo.ArticleMain;
+import com.cn.zbx.pojo.KeyRelation;
+import com.cn.zbx.pojo.KeyWords;
 import com.cn.zbx.pojo.Price;
 import com.cn.zbx.pojo.Tclassify;
 import com.cn.zbx.service.IArticleMainService;
 import com.cn.zbx.util.MapUtil;
 import com.cn.zbx.util.StringUtils;
 import com.cn.zbx.vo.ArticleVO;
+import com.cn.zbx.vo.KeyWordsVO;
 import com.cn.zbx.vo.TClassifyVO;
 
 /**
@@ -35,7 +40,16 @@ public class ArticleMainServiceImpl implements IArticleMainService {
 	
 	@Autowired
 	PriceMapper PriceMapper;
+	
+	@Autowired
+	KeyWordsMapper keyWordsMapper;
+	
+	@Autowired
+	KeyRelationMapper keyRelationMapper;
 
+	/**
+	 * 查询多个 分类对应的文章数
+	 */
 	@Override
 	public List<TClassifyVO> selectCountByClassIds(List<Tclassify> list) {
 		// TODO Auto-generated method stub
@@ -50,6 +64,9 @@ public class ArticleMainServiceImpl implements IArticleMainService {
 		return resultList;
 	}
 
+	/**
+	 * 根据条件查询文章信息
+	 */
 	@Override
 	public List<ArticleVO> selectBySelectParam(ArticleMain record) {
 		// TODO Auto-generated method stub
@@ -57,30 +74,47 @@ public class ArticleMainServiceImpl implements IArticleMainService {
 		return resultList;
 	}
 
+	/**
+	 * 根据条件查询文章数
+	 */
 	@Override
 	public Integer selectCountBySelectParam(ArticleMain record) {
 		// TODO Auto-generated method stub
 		return articleMainMapper.selectCountBySelectParam(record);
 	}
 
+	/**
+	 * 根据主键删除文章信息
+	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, rollbackFor=Exception.class)
 	public int deleteByPrimaryKey(Integer id) {
 		// TODO Auto-generated method stub
 		return articleMainMapper.deleteByPrimaryKey(id);
 	}
 
+	/**
+	 * 根据主键查询文章信息
+	 */
 	@Override
 	public ArticleMain selectByPrimaryKey(Integer id) {
 		// TODO Auto-generated method stub
 		return articleMainMapper.selectByPrimaryKey(id);
 	}
 
+	/**
+	 * 根据主键修改文章信息
+	 */
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, rollbackFor=Exception.class)
 	public int updateByPrimaryKeySelective(ArticleMain record) {
 		// TODO Auto-generated method stub
 		return articleMainMapper.updateByPrimaryKeySelective(record);
 	}
 
+	/**
+	 * 根据条件查询文章信息（不包含文章内容）
+	 */
 	@Override
 	public List<ArticleMain> selectArticleNoContentBySelectParam(
 			ArticleMain record) {
@@ -105,6 +139,9 @@ public class ArticleMainServiceImpl implements IArticleMainService {
 		return resultList;
 	}
 
+	/**
+	 * 根据文章id查询对应的分类（二级和一级分类）
+	 */
 	@Override
 	public Map<String, Object> selectClassifyByArticleId(Integer id) {
 		// TODO Auto-generated method stub
@@ -112,11 +149,12 @@ public class ArticleMainServiceImpl implements IArticleMainService {
 	}
 
 	/**
-	 * 编辑文章信息功能 包括价格
+	 * 编辑文章信息功能 包括价格 和 关键词
+	 * @throws Exception 
 	 */
 	@Override
 	@Transactional(propagation=Propagation.REQUIRED, rollbackFor=Exception.class)
-	public boolean editArticleInfoByVideoId(Map<String, Object> mapParam) {
+	public boolean editArticleInfoByArticleId(Map<String, Object> mapParam) throws Exception {
 		// TODO Auto-generated method stub
 		Date currentDate = new Date();
 		ArticleMain article = new ArticleMain();
@@ -124,46 +162,69 @@ public class ArticleMainServiceImpl implements IArticleMainService {
 		String articlePriceId = String.valueOf(mapParam.get("articlePriceId"));
 		String articlePrice = String.valueOf(mapParam.get("articlePrice"));
 		String articlePriceOld = String.valueOf(mapParam.get("articlePriceOld"));
-		try {
-			article = (ArticleMain)MapUtil.mapToBean(mapParam, article.getClass());
-			int num = articleMainMapper.updateByPrimaryKeySelective(article);
-			
-			if(num > 0 && !articlePrice.equals(articlePriceOld) && article.getIsfree() == 0){
-				if(!StringUtils.isNotEmpty(articlePriceId)){
-					price.setType(1);
-					price.setProductId(article.getId());
-					price.setPrice(Double.valueOf(articlePrice));
-					price.setPlayNumber(0);
-					price.setSumPrice(0.00);
-					price.setAgainPayDays(10);
-					price.setMakedate(currentDate);
-					price.setModifydate(currentDate);
-					int num1 = PriceMapper.insertSelective(price);
-					if(num1 <= 0){
-						return false;
-					}
-				} else {
-					price.setId(Integer.valueOf(articlePriceId));
-					price.setPrice(Double.valueOf(articlePrice));
-					price.setModifydate(currentDate);
-					int num1 = PriceMapper.updateByPrimaryKeySelective(price);
-					if(num1 <= 0){
-						return false;
-					}
-				}
-			} else if(num <= 0){
-				return false;
+		article = (ArticleMain)MapUtil.mapToBean(mapParam, article.getClass());
+		articleMainMapper.updateByPrimaryKeySelective(article);
+		
+		if(!articlePrice.equals(articlePriceOld) && article.getIsfree() == 0){
+			if(!StringUtils.isNotEmpty(articlePriceId)){
+				price.setType(1);
+				price.setProductId(article.getId());
+				price.setPrice(Double.valueOf(articlePrice));
+				price.setPlayNumber(0);
+				price.setSumPrice(0.00);
+				price.setAgainPayDays(10);
+				price.setMakedate(currentDate);
+				price.setModifydate(currentDate);
+				PriceMapper.insertSelective(price);
+				
+			} else {
+				price.setId(Integer.valueOf(articlePriceId));
+				price.setPrice(Double.valueOf(articlePrice));
+				price.setModifydate(currentDate);
+				PriceMapper.updateByPrimaryKeySelective(price);
+				
 			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
+		}
+		if(mapParam.get("keyWords") != null){
+			String[] keyWords = (String[])mapParam.get("keyWords");
+			KeyWords keyWordsParam = null;
+			KeyRelation KeyRelationParam = new KeyRelation();
+			KeyRelationParam.setRelationtype(1);
+			KeyRelationParam.setProductid(article.getId());
+			//修改关键词之前先删除之前所有的关键词
+			keyRelationMapper.deleteBySelective(KeyRelationParam);
+			for(int i=0; i<keyWords.length; i++){
+				int keyWordsId = 0;
+				keyWordsParam = new KeyWords();
+				KeyRelationParam = new KeyRelation();
+				keyWordsParam.setName(keyWords[i]);
+				List<KeyWordsVO> keyWordsList = keyWordsMapper.selectBySelectParam(keyWordsParam);
+				if(keyWordsList != null && keyWordsList.size() != 0){
+					keyWordsId = keyWordsList.get(0).getId();
+				} else {
+					keyWordsParam.setState(1);
+					keyWordsParam.setMakedate(currentDate);
+					keyWordsParam.setModifydate(currentDate);
+					keyWordsMapper.insertSelective(keyWordsParam);
+					keyWordsId = keyWordsParam.getId();
+				}
+				KeyRelationParam.setKeywordsid(keyWordsId);
+				KeyRelationParam.setRelationtype(1);
+				KeyRelationParam.setProductid(article.getId());
+				//添加新的关键词
+				keyRelationMapper.insert(KeyRelationParam);
+			}
+		} else {//修改关键词时如果不输入则删除之前所有的关键词
+			KeyRelation KeyRelationParam = new KeyRelation();
+			KeyRelationParam.setRelationtype(1);
+			KeyRelationParam.setProductid(article.getId());
+			keyRelationMapper.deleteBySelective(KeyRelationParam);
 		}
 		return true;
 	}
 
 	/**
-	 * 新增文章信息 包括价格
+	 * 新增文章信息 包括价格 和 关键词
 	 * @throws Exception 
 	 */
 	@Override
@@ -184,17 +245,95 @@ public class ArticleMainServiceImpl implements IArticleMainService {
 			price.setPlayNumber(0);
 			price.setSumPrice(0.00);
 			price.setAgainPayDays(10);
-//			price.setMakedate(currentDate);
-//			price.setModifydate(currentDate);
-			throw new RuntimeException("hehe"); 
-//			int num1 = PriceMapper.insertSelective(price);
-//			if(num1 <= 0){
-//				return false;
-//			}
+			price.setMakedate(currentDate);
+			price.setModifydate(currentDate);
+			int num1 = PriceMapper.insertSelective(price);
+			if(num1 <= 0){
+				return false;
+			}
 		} else if(num <= 0){
 			return false;
 		}
+		if(mapParam.get("keyWords") != null){
+			String[] keyWords = (String[])mapParam.get("keyWords");
+			KeyWords keyWordsParam = null;
+			KeyRelation KeyRelationParam = null;
+			for(int i=0; i<keyWords.length; i++){
+				int keyWordsId = 0;
+				keyWordsParam = new KeyWords();
+				KeyRelationParam = new KeyRelation();
+				keyWordsParam.setName(keyWords[i]);
+				List<KeyWordsVO> keyWordsList = keyWordsMapper.selectBySelectParam(keyWordsParam);
+				if(keyWordsList != null && keyWordsList.size() != 0){
+					keyWordsId = keyWordsList.get(0).getId();
+				} else {
+					keyWordsParam.setState(1);
+					keyWordsParam.setMakedate(currentDate);
+					keyWordsParam.setModifydate(currentDate);
+					keyWordsMapper.insertSelective(keyWordsParam);
+					keyWordsId = keyWordsParam.getId();
+				}
+				KeyRelationParam.setKeywordsid(keyWordsId);
+				KeyRelationParam.setRelationtype(1);
+				KeyRelationParam.setProductid(article.getId());
+				keyRelationMapper.insert(KeyRelationParam);
+			}
+		}
 		return true;
+	}
+
+	/**
+	 * 查询文章数，没有关联视频的文章，返回信息没有文章内容
+	 */
+	@Override
+	public Integer selectCountBySelectParamNoRelationVideo(ArticleMain record) {
+		// TODO Auto-generated method stub
+		return articleMainMapper.selectCountBySelectParamNoRelationVideo(record);
+	}
+
+	/**
+	 * 查询文章信息，没有关联视频的文章，返回信息没有文章内容
+	 */
+	@Override
+	public List<ArticleMain> selectArticleNoContentBySelectParamNoRelationVideo(
+			ArticleMain record) {
+		// TODO Auto-generated method stub
+		List<ArticleMain> resultList = new ArrayList<ArticleMain>();
+		List<ArticleVO> articleVOList = articleMainMapper.selectBySelectParamNoRelationVideo(record);
+		ArticleMain oo;
+		for(ArticleVO o : articleVOList){
+			oo = new ArticleMain();
+			oo.setId(o.getId());
+			oo.setTitle(o.getTitle());
+			oo.setImgurl(o.getImgurl());
+			oo.setClassid(o.getClassid());
+			oo.setExcerpt(o.getExcerpt());
+			oo.setIsfree(o.getIsfree());
+			oo.setNocomment(o.getNocomment());
+			oo.setState(o.getState());
+			oo.setMakedate(o.getMakedate());
+			oo.setModifydate(o.getModifydate());
+			resultList.add(oo);
+		}
+		return resultList;
+	}
+
+	/**
+	 * 查询文章信息，添加关键词查询
+	 */
+	@Override
+	public List<ArticleVO> selectBySelectParamV2(ArticleVO record) {
+		// TODO Auto-generated method stub
+		return articleMainMapper.selectBySelectParamV2(record);
+	}
+
+	/**
+	 * 查询文章数，添加关键词查询
+	 */
+	@Override
+	public Integer selectCountBySelectParamV2(ArticleVO record) {
+		// TODO Auto-generated method stub
+		return articleMainMapper.selectCountBySelectParamV2(record);
 	}
 
 }
