83fb1fc12097dded6a828f62fd4adaf43dd38fbd
zhangdongliang <1061650793@qq.com>
2017-11-27 16:35:38 +0800

    统计函数修改 zdl

--- docment/Function/function_statistics.sql
+++ docment/Function/function_statistics.sql
@@ -1,7 +1,7 @@
--- dstart 开始时间 dend 结束时间 dflag[hour|day|month|year] 时间间隔
+-- dstart 开始时间; dend 结束时间; dflag[hour|day|month|year] 时间间隔; ptype[1|2] 统计类型：1-文章 2-视频
 -- 返回每个时间段的时间和数量字符串 eg：时间1:数量1|时间2:数量2|时间3:数量3...
 Drop function if exists function_statistics;
-CREATE FUNCTION function_statistics(dstart TIMESTAMP, dend TIMESTAMP, dflag VARCHAR(10))
+CREATE FUNCTION function_statistics(dstart TIMESTAMP, dend TIMESTAMP, dflag VARCHAR(10), ptype INT)
 RETURNS VARCHAR(1000)
 BEGIN
 	DECLARE dateBox TIMESTAMP;
@@ -15,7 +15,10 @@ BEGIN
 		WHEN dflag = 'year' THEN SET dformat = '%Y';
 	END CASE;
 	WHILE dateBox <= dend DO 
-		SELECT CONCAT(resultMap,'|',DATE_FORMAT(dateBox,dformat),':',count(1)) INTO resultMap FROM statistics WHERE DATE_FORMAT(create_date,dformat) = DATE_FORMAT(dateBox,dformat);
+		SELECT CONCAT(resultMap,'|',DATE_FORMAT(dateBox,dformat),':',count(1)) INTO resultMap 
+		FROM statistics 
+		WHERE DATE_FORMAT(create_date,dformat) = DATE_FORMAT(dateBox,dformat)
+		AND product_type = ptype;
 		CASE 
 			WHEN dflag = 'hour' THEN set dateBox = date_add(dateBox, interval 1 hour);
 			WHEN dflag = 'day' THEN set dateBox = date_add(dateBox, interval 1 day);
